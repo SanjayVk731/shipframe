f9efbcae7ba8f61616f15b392922e6ab3aa198ea
DrLector666 <97535642+DrLector666@users.noreply.github.com>
2026-05-19 15:06:09 +0530

    fix(build): set sandbox target to es2017 (QuickJS doesn't support optional catch binding)

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(() => {
       build: {
         outDir: 'dist',
         emptyOutDir: false,
+        target: 'es2017',
         lib: {
           entry: resolve(__dirname, 'src/sandbox/main.ts'),
           formats: ['iife'],
